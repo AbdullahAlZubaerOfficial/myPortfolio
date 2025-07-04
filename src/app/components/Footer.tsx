92f105e1e443752704938f3cca08d2166a32a2bd
Zubaer3002 <your-email@example.com>
2025-07-05 01:16:31 +0600

    updated

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from 'next/link'
 import { FaGithub, FaTwitter, FaLinkedin } from 'react-icons/fa'
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+// ❌ DON'T add "use client" here
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -6,33 +8,32 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
   subsets: ["latin"],
+  variable: "--font-geist-sans",
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
+  variable: "--font-geist-mono",
 });
 
 export const metadata: Metadata = {
   title: "Devfolio | Portfolio Website using Next.js, Tailwind CSS, and Framer Motion",
-  description: "Devfolio is a portfolio website for developers to showcase their projects and skills.",
+  description:
+    "Devfolio is a portfolio website for developers to showcase their projects and skills.",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-    <body className={`bg-white transition-colors dark:bg-gray-900 dark:text-white ${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className="bg-white transition-colors dark:bg-gray-900 dark:text-white">
         <ThemeProvider>
           <Navbar />
-          <main className="min-h-screen pt-24">
-            {children}
-          </main>
+          <main className="min-h-screen pt-24">{children}</main>
           <Footer />
         </ThemeProvider>
       </body>
