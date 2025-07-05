3f6fd12004987a0265a6a2f6e385e2f17e54d409
Zubaer3002 <your-email@example.com>
2025-07-05 19:37:43 +0600

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,10 @@ export const metadata: Metadata = {
   title: "Portfolio | Abdullah Al Zubaer Mern Stack Developer",
   description:
     "Hi! I’m Abdullah Al Zubaer, a passionate MERN Stack Web Developer, Digital Marketer, and lifelong learner from Bangladesh 🇧🇩. With over 50+ completed projects for clients worldwide 🌍, I specialize in building responsive, dynamic, and high-performance web applications using React.js, Next.js, Firebase, MongoDB, and Tailwind CSS.I am also experienced in SEO, social media marketing, and content creation — including video and image editing to enhance digital presence. I’m currently pursuing my BSc in Computer Science and Engineering (CSE), aiming to become a Data Scientist and work at top companies like Google or Microsoft 🚀.Besides coding, I love teaching students (Class 1–10), running home tuition services, and exploring new technologies. My goal is to combine my skills in development, marketing, and education to build impactful products and empower others 💡.",
-};
+      icons: {
+    icon: "/profile-pic.png", // or "/logo.png", or "/logo.svg"
+  },
+  };
 
 export default function RootLayout({
   children,
