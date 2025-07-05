9a0d4b2b7cbde1108f371f5475a69887908dbac7
Zubaer3002 <your-email@example.com>
2025-07-05 14:55:48 +0600

    updated

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -38,7 +38,7 @@ export const pageTransition: Variants = {
   initial: { opacity: 0 },
   animate: { opacity: 1 },
   exit: { opacity: 0 },
-  transition: { duration: 0.5 } as Transition,
+  // transition: { duration: 0.5 } as Transition,
 };
 
 // Slide In from Left Animation
